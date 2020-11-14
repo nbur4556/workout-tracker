d1ac74ec000af0f28e63f1632f8d5332fac1058d
nbur4556 <nbur4556@gmail.com>
2020-11-14 12:47:42 -0600

    createx express routes to home, stats, and exercise

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send('/');
+});
+
+app.get('/stats', (req, res) => {
+    res.send('/stats');
+});
+
+app.get('/exercise', (req, res) => {
+    res.send('/exercise');
+});
+
 app.listen(PORT, () => {
     console.log("Listening on http://localhost:" + PORT);
-})
+});
