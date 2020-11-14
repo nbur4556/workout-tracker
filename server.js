0ad982c9879d22ffdd307dd9d5d45efe89588f0a
nbur4556 <nbur4556@gmail.com>
2020-11-14 12:55:17 -0600

    express uses public folder and routes to corrent html files

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Middleware
+app.use(express.static('public'));
+
+// Routes
 app.get('/', (req, res) => {
-    res.send('/');
+    res.sendFile(`${__dirname}\\public\\index.html`);
 });
 
 app.get('/stats', (req, res) => {
-    res.send('/stats');
+    res.sendFile(`${__dirname}\\public\\stats.html`);
 });
 
 app.get('/exercise', (req, res) => {
-    res.send('/exercise');
+    res.sendFile(`${__dirname}\\public\\exercise.html`);
 });
 
+// Server listen
 app.listen(PORT, () => {
     console.log("Listening on http://localhost:" + PORT);
 });
