fa88543d6d3f490eefc87ab97a58c347992eec6b
nbur4556 <nbur4556@gmail.com>
2020-11-17 10:54:20 -0600

    fix for database object

--- models/database.js
+++ models/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     CardioExercise: require('./CardioExercise.js'),
     ResistanceExercise: require('./ResistanceExercise.js'),
-    Workout: require('./Workout.js');
+    Workout: require('./Workout.js')
 }
