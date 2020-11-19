3d3008570304086a55037b24912f0c8189040334
nbur4556 <nbur4556@gmail.com>
2020-11-19 16:45:52 -0600

    removes forward slashes from html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,12 @@
 module.exports = (app, __root) => {
     // Home Page
     app.get('/', (req, res) => {
-        res.sendFile(`${__root}\\public\\index.html`);
+        res.sendFile(`${__root}/public/index.html`);
     });
 
     // Stats Page
     app.get('/stats', (req, res) => {
-        res.sendFile(`${__root}\\public\\stats.html`);
+        res.sendFile(`${__root}/public/stats.html`);
     });
 
     // Exercise Page
